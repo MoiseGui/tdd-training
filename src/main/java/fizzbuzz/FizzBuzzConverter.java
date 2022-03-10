d4b2efaed98f199ee0a0eddacd6b870e6570fa72
Moïse Gui <k.moise.gui@gmail.com>
2022-03-10 17:04:31 +0100

    refactoring the order of the ifs

--- src/main/java/fizzbuzz/FizzBuzzConverter.java
+++ src/main/java/fizzbuzz/FizzBuzzConverter.java
@@ -3,8 +3,8 @@ package fizzbuzz;
 public class FizzBuzzConverter {
     public static String convert(int i) {
         if(i % 15 == 0) return "FizzBuzz";
-        if(i % 3 == 0) return "Fizz";
         if(i % 5 == 0) return "Buzz";
+        if(i % 3 == 0) return "Fizz";
         return String.valueOf(i);
     }
 }
